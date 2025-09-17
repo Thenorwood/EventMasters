512d56fdd3eefaad85c0a7806c5c2f79ae3dc0c1
Bryn Norwood <Bryn.Norwood@nscc.ca>
2025-09-17 13:01:18 -0300

    Add project files.

---
+++ EventMasters/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿namespace EventMasters.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+
+
+
+    }
+}
---
+++ EventMasters/Models/Venue.cs
@@ -0,0 +1,14 @@
+﻿namespace EventMasters.Models
+{
+    public class Venue
+    {
+        public int VenueId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Location { get; set; } = string.Empty;
+
+        public int Capacity { get; set; }   
+
+    }
+}
