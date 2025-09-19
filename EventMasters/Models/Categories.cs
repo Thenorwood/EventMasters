7c8925faf6a2c86382483050f53f517868435b8a
Bryn Norwood <Bryn.Norwood@nscc.ca>
2025-09-19 16:11:29 -0300

    new commit

---
+++ EventMasters/Models/Categories.cs
@@ -0,0 +1,15 @@
+﻿namespace EventMasters.Models
+{
+    public class Categories
+    {
+        public int CategoryId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Type{ get; set; } = string.Empty;
+
+
+
+
+    }
+}
