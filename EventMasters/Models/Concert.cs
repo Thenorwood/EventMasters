82653bf939498aaa287e8c95829601bdc974f166
Bryn Norwood <Bryn.Norwood@nscc.ca>
2025-09-25 13:05:01 -0300

    changed event to concert

---
+++ EventMasters/Models/Concert.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace EventMasters.Models
+{
+    public class Concert
+    {
+        public int ConcertId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime Date { get; set; }
+
+        public int VenueId { get; set; }
+
+        public int CategoryId { get; set; } 
+
+
+
+    }
+}
