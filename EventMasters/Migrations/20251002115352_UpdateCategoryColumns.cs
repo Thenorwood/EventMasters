2ae45bf785e533be7ae3e48f2c0223d2b7e86322
Bryn Norwood <Bryn.Norwood@nscc.ca>
2025-10-02 09:26:22 -0300

    edit spacing

---
+++ EventMasters/Migrations/20251002115352_UpdateCategoryColumns.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventMasters.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCategoryColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Category",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Category",
+                newName: "Description");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Category",
+                newName: "Type");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Category",
+                newName: "Title");
+        }
+    }
+}
